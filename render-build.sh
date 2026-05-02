59802e50d9976b788d1e60a70f4fc34846ca15dd
uniquehere00 <uniquedas00@gmail.com>
2026-05-02 21:32:51 +0530

    switch to nixpacks for railway

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-apt-get update -y
-apt-get install -y tesseract-ocr poppler-utils libgl1-mesa-glx
-python3 -m pip install --upgrade pip
-python3 -m pip install -r backend/requirements.txt

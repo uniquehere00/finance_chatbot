390dce23336fcd14b41b3391c921cc0296160cd5
uniquehere00 <uniquedas00@gmail.com>
2026-05-02 21:26:23 +0530

    fixed with  python3  command for railway

--- render-build.sh
+++ render-build.sh
@@ -2,5 +2,5 @@
 set -e
 apt-get update -y
 apt-get install -y tesseract-ocr poppler-utils libgl1-mesa-glx
-python -m pip install --upgrade pip
-python -m pip install -r backend/requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r backend/requirements.txt
